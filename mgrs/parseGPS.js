b8119aec86ded0796835eb50e85132ef816ed4f9
Nicholas Fong <nicholas.fong@gmail.com>
2024-08-10 22:46:34 -0700

    Update parseGPS.js

--- mgrs/parseGPS.js
+++ mgrs/parseGPS.js
@@ -27,16 +27,16 @@ export function parseGPS(inputString) {
     const p1 = new LatLon(latitude, longitude);
     console.log(p1.toString('dms'));
     console.log(p1.toString('dm'));
-    document.getElementById('response1').textContent = p1.toString('dms');
-    document.getElementById('response2').textContent = p1.toString('dm');
+    document.getElementById('response1').textContent = `DMS: ${p1.toString('dms')}`;
+    document.getElementById('response2').textContent = `DM: ${p1.toString('dm')}`;
 
     const point = new LatLon(latitude, longitude);
     const utm = point.toUtm();
     const mgrs = utm.toMgrs();
     console.log(mgrs);
     console.log(utm);
-    document.getElementById('response3').textContent = mgrs.toString(10).replace(/ /g, '');
-    document.getElementById('response4').textContent = utm;
+    document.getElementById('response3').textContent = `MGRS: ${mgrs.toString(10).replace(/ /g, '')}`;
+    document.getElementById('response4').textContent = `UTM N/S hemisphere notation ${utm}`;
 
     // double back check: convert MGRS back to latitude longitude
     const latlon_from_mgrs = mgrs.toUtm().toLatLon();
