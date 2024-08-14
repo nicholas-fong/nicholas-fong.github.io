d2f72de1a68106c0b36dc0a2bb92b1563484936e
Nicholas Fong <nicholas.fong@gmail.com>
2024-08-13 19:16:49 -0700

    Update parseMGRS.js

--- mgrs/parseMGRS.js
+++ mgrs/parseMGRS.js
@@ -1,5 +1,5 @@
-//parseMGRS.js
 import Mgrs from './mgrs.js';
+import { LatLon } from './mgrs.js';
 
 export function parseMGRS(mgrsString) {
 
@@ -10,16 +10,32 @@ export function parseMGRS(mgrsString) {
     if (pattern.test(cleanMGRS)) {
         console.log(`clean up MGRS ${cleanMGRS}`);
         console.log("regex check passed")
-        document.getElementById('message2').textContent = ""
+        document.getElementById('message1').textContent = ""
 
-        const mgrs = Mgrs.parse(cleanMGRS);
+        const mgrs = Mgrs.parse(cleanMGRS); 
         const LATLON = mgrs.toUtm().toLatLon();
+        const latitude = LATLON.lat; 
+        const longitude = LATLON.lon;
+
+        const point = new LatLon(latitude, longitude);
+        const utm = point.toUtm();
+        const STD_UTM = `${utm['zone']} ${utm['hemisphere']} ${parseInt(utm['easting']).toString()} ${parseInt(utm ['northing']).toString()}   `;
+        const NATO_UTM = `${utm['zone']} ${mgrs['band']} ${parseInt(utm['easting']).toString()} ${parseInt(utm ['northing']).toString()}` ;
+
+        console.log(latitude, longitude);
+        console.log(mgrs);
+        console.log(utm);
 
         document.getElementById('response5').textContent = `${LATLON.lat.toFixed(5)},   ${LATLON.lon.toFixed(5)}` ;
         document.getElementById('response6').textContent = LATLON.toString();
-        document.getElementById('message2').textContent = `MGRS:  ${cleanMGRS}`;
+        document.getElementById('response7').textContent = `UTM Band Notation:  ${NATO_UTM}`;
+        document.getElementById('response8').textContent = `UTM Hemi Notation:  ${STD_UTM}`;
     } else {
         console.log("Invalid MGRS string.");
-        document.getElementById('message2').textContent = "Invalid MGRS string, hit Refresh and try again"
+        document.getElementById('response5').textContent = "" ;
+        document.getElementById('response6').textContent = "";
+        document.getElementById('response7').textContent = "";
+        document.getElementById('response8').textContent = "";
+        document.getElementById('message1').textContent = "Invalid MGRS string, hit Refresh and try again"
     }
 }
