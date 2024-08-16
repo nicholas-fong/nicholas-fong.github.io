c9bd701c5eb416c58300ac26d0377e0a4588d85b
Nicholas Fong <nicholas.fong@gmail.com>
2024-08-15 19:04:32 -0700

    update labels

--- interest-rate/version2/calculator2.js
+++ interest-rate/version2/calculator2.js
@@ -7,21 +7,19 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
   });
 
 function handleForm () {
-    const principal = parseFloat(document.getElementById('principal').value);
-    const finalAmount = parseFloat(document.getElementById('finalAmount').value);
+    let principal = document.getElementById('principal').value;
+    principal = principal.replace(/,/g,"");
+    principal = parseFloat(principal);
+    let finalAmount = document.getElementById('finalAmount').value;
+    finalAmount = finalAmount.replace(/,/g,"");
+    finalAmount = parseFloat(finalAmount);
     const years = parseFloat(document.getElementById('years').value, 10);
     const timesInput = document.getElementById('times').value;
-    const result = document.getElementById('result'); // Ensure result is defined
-
     const times = timesInput === "" ? 1 : parseInt(timesInput, 10);
-
-    if (isNaN(principal) || isNaN(finalAmount) || isNaN(years) || isNaN(times)) {
-        result.textContent = 'Please enter valid numbers in Principal, Final Amount, Years';
-        return;
-    }
-
+    const result = document.getElementById('result');    
+    
     const rate = calculateAnnualInterestRate(principal, finalAmount, times, years);
-    result.textContent = `The annual interest rate is ${rate}%.`;
+    result.textContent = `Annualized ROI ${rate}%.`;
 };
 
 function calculateAnnualInterestRate (principal, finalAmount, times, years) {
