d8118b9bdfd5265f2e902eb7d2472f0b85fbef3a
Nicholas Fong <nicholas.fong@gmail.com>
2024-08-16 12:01:47 -0700

    re-organize folders

--- interest-rate/version3/utilities.js
+++
@@ -1,5 +0,0 @@
-// utilities.js
-export const calculateAnnualInterestRate = (principal, finalAmount, times, years) => {
-    const rate = times * (Math.pow(finalAmount / principal, 1 / (times * years)) - 1) * 100;
-    return rate.toFixed(2);
-};
